affa0157790bc6891b0c9d5b86d87598fe21151e
Italo Hugo <italohugo625@gmail.com>
2023-11-22 08:37:41 -0300

    New files

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
-# Footprinting_with_Python
+# Footprinting with Python  
+
+---
+
+This is a tool used for footprinting web applications by retrieving the application's headers.  
+
+This project is also a skill showcase. For this showcase, the [tool][link1] was used to footprint an application running
+within a **Docker** container. The image used was a [OWASP WebGoat][link2] container running on [Docker][link3].  
+
+The concept for this project was based on the [Coursera Guided Project][link4]. Although, the source code present here
+was created, debugged, and tested by the owner of this repository.  
+
+For the tool's functionality, you can go click here: [Tool][link5]
+
+---  
+
+### **How to use the tool**  
+
+The tool was designed to be used on linux's terminal.  
+
+You can use it by downloading source code on [footprint][code],
+which can be run from anywhere by adding the file to the `/bin` folder.  
+
+Otherwise, you can download the Python 3 source
+code on [footprint.py][pycode], using this code you'll need to execute the command `python3 ./footprint.py [args]`.  
+
+Alternatively, you can clone this repository by executing the following command:  
+
+> `git clone https://github.com/ItaloHugoMDS/Footprinting_with_Python.git`  
+
+---  
+
+### **Syntax**  
+
+The tool requires a few arguments to be run successfully.  
+
+You can either specify a single URL while executing the script. For example:  
+
+> `./footprint "https://google.com"`  
+
+If you are using the Python 3 script:  
+
+> `python3 footprint.py "https://google.com"`  
+
+However, the tool also accepts multiple URLs at once. To run the script through multiple URLs, you should create a file
+listing them. You can see an example of such a file under [recon.txt][file].  
+
+After creating a file with the URLs, just like in [recon.txt][file], you can execute the following command:  
+
+> `./footprint [URL_list.txt]`  
+
+In case you use the Python 3 script, you can run:  
+
+> `python3 footprint.py [URL_list.txt]`  
+
+To run the code using the ".txt" file from this repository, you can run:  
+
+> `./footprint recon.txt`  
+
+Using the Python 3 script:  
+
+> `python3 footprint recon.txt`  
+
+The output will look similar to this:  
+
+> [![Script Output 1](images/Footprinting_File_1.png "Script Output 1")][image1]  
+
+> [![Script Output 2](images/Footprinting_File_2.png "Script Output 2")][image2]  
+
+> [![Script Output 3](images/Footprinting_File_3.png "Script Output 3")][image3]  
+
+---  
+
+## Showcase  
+
+For this showcase, a **Docker** container running a version of **WebGoat** was started to be footprinted 
+
+[link1]: https://github.com/ItaloHugoMDS/Footprinting_with_Python "Footprinting tool"
+[link2]: https://github.com/WebGoat/WebGoat "WebGoat"
+[link3]: https://www.docker.com/ "Docker"
+[link4]: https://www.coursera.org/projects/write-your-own-python-tool-to-footprint-a-web-application "Project source"
+[link5]: 
+[code]: https://github.com/ItaloHugoMDS/Footprinting_with_Python/blob/main/footprint "Source code"
+[pycode]: https://github.com/ItaloHugoMDS/Footprinting_with_Python/blob/main/footprint.py "Souce '.py' code"
+[file]: https://github.com/ItaloHugoMDS/Footprinting_with_Python/blob/main/recon.txt "File example"
+[image1]: https://github.com/ItaloHugoMDS/Footprinting_with_Python/blob/main/images/Footprinting_File_1.png
+[image2]: https://github.com/ItaloHugoMDS/Footprinting_with_Python/blob/main/images/Footprinting_File_2.png
+[image3]: https://github.com/ItaloHugoMDS/Footprinting_with_Python/blob/main/images/Footprinting_File_3.png
--- footprint.py
+++ footprint.py
@@ -4,6 +4,30 @@ import sys
 import validators
 
 
+def url_eval(url):
+    """Evaluating if the URL is valid and making the request."""
+
+    if not validators.url(url):  # Evaluating if it's a valid URL.
+        print(f"{url} is an invalid url to be used.\n\n")  # Print an error message in case invalid URL.
+        exit(3)  # End the program with status code 3.
+
+    try:
+
+        page = requests.get(url)  # Trying to make a request to the current URL.
+
+    except requests.RequestException:  # In case of any request exceptions:
+
+        print(f"Error: Bad URL request for {url}")  # Printing a message for a Bad Request Error.
+        exit(4)  # Ending the program with status code 4.
+
+    if page.status_code != 200:  # Evaluating if the request was successful if the request code is different
+        # then 200 (Code for successful request):
+        page.raise_for_status()  # Printing an error message with the returned status code.
+        exit(5)  # Ending the program execution with status code 5.
+
+    return page
+
+
 def fetch_headers(given_url):
     """Fetching the headers from the given URL(s)"""
 
@@ -11,50 +35,39 @@ def fetch_headers(given_url):
 
     if os.path.isfile(given_url):   # Checking if the passed argument is a file:
 
-        urls = []
+        urls = []   # This list will store the URL(s) from the file passed as an argument.
 
-        with open(f"{os.path.abspath(given_url)}", "r") as file:
-            lines = file.readlines()
-            for line in lines:
-                if line != "\n":
-                    urls.append(line.strip())
+        with open(f"{os.path.abspath(given_url)}", "r") as file:    # Opening the file.
+            lines = file.readlines()    # Read the lines into program memory.
+            for line in lines:  # Going through each URL.
+                if line != "\n":    # Making sure only valid URL are loaded into the list, ignoring empty lines.
+                    urls.append(line.strip())   # Adding the URL into the "urls" lists and removing any extra characters
 
-        file.close()
+        file.close()    # Closing the file.
 
-        for url in urls:
+        for url in urls:    # Iterating through each URL into the list
 
-            if not validators.url(url):
-                print(f"{url} is an invalid url to be used.\n\n")
-                exit(3)
+            page = url_eval(url)    # Evaluating if it's a valid URL.
 
-            page = requests.get(url)
+            headers_dict[url] = page.headers    # If the request was successful, adding the url and the url's header to
+            # the "headers_dict" dictionary for processing the output message later.
 
-            if page.status_code != 200:
-                page.raise_for_status()
-                exit(4)
+    else:   # If the passed argument is not a file, it means it's just a single URL, therefore:
 
-            headers_dict[url] = page.headers
+        page = url_eval(given_url)   # Evaluating if it's a valid URL.
 
-    else:   # If the passed argument is not a file, it means it's just a single URL:
+        headers_dict[given_url] = page.headers  # If the request was successful, adding the url and the url's header to
+        # the "headers_dict" dictionary for processing the output message later.
 
-        if not validators.url(given_url):
-            print(f"{given_url} is an invalid url to be used.\n\n")
-            exit(3)
-
-        page = requests.get(given_url)
-
-        if page.status_code != 200:
-            page.raise_for_status()
-
-        headers_dict[given_url] = page.headers
-
-    return headers_dict
+    return headers_dict    # Returning the dictionary containing the URL(s) and the Header(s) as key-value pairs,
+    # respectively.
 
 
 def processing_headers(given_header):
     """Processing the output for the URL(s) header(s)"""
 
-    for url, header in given_header.items():
+    for url, header in given_header.items():    # Iterating through each "url-header" element within the dictionary
+        # passed as an argument to the function.
 
         print("\n")
         print(f"{'*' * len(url)}****************")
@@ -62,7 +75,8 @@ def processing_headers(given_header):
         print(f"{'*' * len(url)}****************\n\n")
         print("-------------------------------------------------------------------------------------------------------")
         print("\n")
-        for key, value in header.items():
+
+        for key, value in header.items():   # Printing the header's element for the current URL.
 
             print(f"{key}: \n\t{value}\n")
 
@@ -72,22 +86,23 @@ def processing_headers(given_header):
 
 def main():
 
+    # Error message in case argument errors are detected.
     msg = "Please enter use the file path containing the urls\n"\
           "or enter the a simple url."
 
-    if not sys.argv[1]:
-        print(f"Invalid arguments\n\n{msg}")
-        exit(1)
+    if not sys.argv[1]:    # Checking if one argument was provided when running the script from the terminal.
+        print(f"Invalid arguments\n\n{msg}")    # Printing an error message in case missing argument.
+        exit(1)    # Ending program's execution with status code 1.
 
-    if len(sys.argv) > 2:
-        print(f"Too many arguments!\n\n{msg}")
-        exit(2)
+    if len(sys.argv) > 2:   # Checking if more than one argument was used.
+        print(f"Too many arguments!\n\n{msg}")  # Printing an error message in case too many arguments.
+        exit(2)    # Ending program's execution with status code 2.
 
-    headers = fetch_headers(sys.argv[1])
+    headers = fetch_headers(sys.argv[1])    # Retrieving the header(s) for the URL(s).
 
-    processing_headers(headers)
+    processing_headers(headers)    # Processing the output for the header(s).
 
 
 if __name__ == "__main__":
 
-    main()
+    main()  # Executing script.
